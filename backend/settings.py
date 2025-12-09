fbc211655721270d61d44ba7a97e3c6139351f93
Dan Store <sanchezdante039@gmail.com>
2025-12-08 20:12:30 -0500

    Forzando DEBUG False para arreglar imagenes

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8)3nl3)x!+54nu+*b7@ba^k5j-6%d-_ek@*@+ao3dz^1gd@_eu')
 
 # Si estamos en RENDER, Debug será False (Seguro). En tu PC será True.
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
