9968e8489a9caf02bdf246b1189cf7d75f40636f
Dan Store <sanchezdante039@gmail.com>
2025-12-08 19:32:10 -0500

    Instalando Cloudinary y configurando settings

--- backend/settings.py
+++ backend/settings.py
@@ -134,9 +134,17 @@ CORS_ALLOW_ALL_ORIGINS = True
 # ==========================================
 # 7. MULTIMEDIA (FOTOS)
 # ==========================================
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+else:
+    # Si estamos en PC, usamos carpeta normal
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # ==========================================
